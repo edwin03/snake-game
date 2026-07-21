7391d5f42a1fa46b676c27800c495ff48092f6d1
Edwin <edwin.rubio@toshiba.com>
2026-07-21 14:01:25 -0500

    Completed part 1.

--- main.py
+++ main.py
@@ -7,19 +7,25 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
+screen.listen()
 segments = []
 
-starting_pos = [(0, 0), (-20, 0), (-40, 0)]
+snake = Snake()
 
-for _ in range(3):
-    snake = Snake(starting_pos[_])
-    segments.append(snake)
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.up, "w")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.left, "a")
+screen.onkey(snake.right, "d")
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    segments[0].move(segments)
+    snake.move()
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -1,15 +1,46 @@
 from turtle import Turtle
+STARTING_POS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DIST = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
-    def __init__(self, position):
-        self = Turtle("square")
-        self.penup()
-        self.color("white")
-        self.goto(position)
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
     
-    def move(self, segments):
-        for seg in range(len(segments)-1, 0, -1):
-            new_x = segments[seg - 1].xcor()
-            new_y = segments[seg - 1].ycor()
+    def create_snake(self):
+        for position in STARTING_POS:
+            new_seg = Turtle("square")
+            new_seg.color('white')
+            new_seg.penup()
+            new_seg.goto(position)
+            self.segments.append(new_seg)
+    
+    def move(self):
+        for seg in range(len(self.segments)-1, 0, -1):
+            new_x = self.segments[seg - 1].xcor()
+            new_y = self.segments[seg - 1].ycor()
             self.segments[seg].goto(new_x, new_y)
-        self.segments[0].forward(20)
+        self.segments[0].forward(MOVE_DIST)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.seth(UP)
+    
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.seth(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.seth(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.seth(RIGHT)
+        
