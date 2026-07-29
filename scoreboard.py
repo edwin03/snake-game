2b366edc8c923d69f52af9d07615f3b574ded50d
Edwin <edwin.rubio@toshiba.com>
2026-07-29 17:09:22 -0500

    Adding the presistent high score feature.

--- scoreboard.py
+++ scoreboard.py
@@ -5,6 +5,7 @@ FONT = ("monospace", 18, "normal")
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+        self.high_score = 0
         self.score = 0
         self.penup()
         self.goto(0, 270)
@@ -15,14 +16,20 @@ class Scoreboard(Turtle):
     def update_scoreboard(self):
         self.clear()
         self.goto(0, 270)
-        self.write(f"SCORE: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"SCORE: {self.score} HIGH SCORE: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def score_up(self):
         self.score += 1
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
         
     
